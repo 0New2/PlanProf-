7cce85b8d4d68f80865362118a5ed60746c447e1
0New2 <lgn.corentin@gmail.com>
2026-01-20 18:20:32 +0100

    Delete js directory

--- js/script.js
+++
@@ -1,9 +0,0 @@
-// ========================================
-// PLANPROF - JAVASCRIPT
-// ========================================
-
-// Fichier JS pour les futures fonctionnalités
-
-console.log('PlanProf chargé ! 🎨');
-
-// Tu ajouteras tes fonctions ici plus tard
